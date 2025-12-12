51d7ed647ca8eaa2f1afa24d9ab6de5984142f5b
Sandro8173 <sandrogtr@outlook.com>
2025-12-11 22:24:32 -0300

    Add files via upload

---
+++ site filmes/js/index.js
@@ -0,0 +1,8 @@
+const hamburguer = document.querySelector('.hamburguer');
+const nav = document.querySelector('.nave');
+
+hamburguer.addEventListener("click", () => 
+ nav.classList.toggle("active")
+);
+  
+
